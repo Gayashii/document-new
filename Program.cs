2436ecf654b5b753fd97605da82aab7bea890765
Malith Gammanpila <ssmalith93@gmail.com>
2026-04-01 21:48:20 +0400

    fix CORS

--- Program.cs
+++ Program.cs
@@ -75,7 +75,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors("AllowAngular");
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+	app.UseHttpsRedirection();
+}
 app.UseAuthorization();
 app.MapControllers();
 
